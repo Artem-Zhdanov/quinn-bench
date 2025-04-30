0c2361787196ca642decd8794251005bb3b38cab
Artem Zhdanov <artem.a.zhdanov@gmail.com>
2025-04-30 19:31:03 +0500

    2 connections

--- src/publisher.rs
+++ src/publisher.rs
@@ -8,18 +8,16 @@ use crate::now_ms;
 use crate::quic_config::configure_client;
 
 pub async fn run(addr: String, port: u16) -> Result<()> {
-    let config = configure_client()?;
-    let url = format!("https://{}:{}", addr, port);
-    let connection = Endpoint::client(config)?.connect(url).await?;
-
     for _ in 0..2 {
-        let conn_clone = connection.clone();
+        let config = configure_client()?;
+        let url = format!("https://{}:{}", addr, port);
+        let connection = Endpoint::client(config)?.connect(url).await?;
 
         tokio::spawn(async move {
             let mut data = vec![42u8; BLOCK_SIZE];
 
             loop {
-                let mut stream = conn_clone.open_uni().await.unwrap().await.unwrap();
+                let mut stream = connection.open_uni().await.unwrap().await.unwrap();
 
                 data[0..8].copy_from_slice(&now_ms().to_be_bytes());
 
